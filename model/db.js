262275c6d3b7e8a0792246dabfe2425fb825839c
slysop <slysop@gmail.com>
2013-10-30 17:16:01 -0500

    update

--- model/db.js
+++ model/db.js
@@ -1,13 +1,36 @@
-// --------------- CONNECT TO OUR DATABASE
-
-// Bring Mongoose into the project
-var mongoose = require('mongoose');
-
-// Build the connection string
-var dbURI = 'mongodb://localhost/MongoosePM';
-
-//Create the database connection
+var mongoose = require('mongoose'),
+dbURI = 'mongodb://localhost/MongoosePM';
 mongoose.connect(dbURI);
+// Connection events snipped out for brevity
+
+/* ********************************************
+      USER SCHEMA
+   ******************************************** */
+var userSchema = new mongoose.Schema({
+    name: String,
+    email: { type: String, unique: true },
+    createdOn: { type: Date, default: Date.now },
+    modifiedOn: Date,
+    lastLogin: Date
+});
+
+// Build the User model
+mongoose.model('User', userSchema);
+
+/* ********************************************
+      PROJECT SCHEMA
+   ******************************************** */
+var projectSchema = new mongoose.Schema({
+    projectName: String,
+    createdOn: { type: Date, default: Date.now },
+    modifiedOn: Date,
+    createdBy: String,
+    contributors: String,
+    tasks: String
+});
+
+// Build the Project model
+mongoose.model('Project', projectSchema);
 
 // --------------- SET UP EVENT HANDLERS
 mongoose.connection.on('connected', function () {
--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,17 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+
+// GET user creation form
+exports.create = function (req, res) {
+    res.render('user-form', {
+        title: 'Create user',
+        buttonText: "Join!"
+    });
+};
+
+exports.index = function (req, res) {
+    res.render('user-index', {
+        title: 'Users',
+        buttonText: "Hello"
+    });
+};
